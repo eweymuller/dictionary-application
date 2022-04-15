a3696ebbaac3ff6434514a81c70827a8f383dc6c
eweymuller <eweymuller@gmail.com>
2022-04-15 17:01:52 +0200

    Connected API to search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for the definition of ${keyWord}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeyWordChange(event) {
     setKeyWord(event.target.value);
