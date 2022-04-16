224182701f6246ae7ce67402e39abc593925d2dc
eweymuller <eweymuller@gmail.com>
2022-04-16 10:11:29 +0200

    + Footer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <footer>
+      This project was coded by Eva Weymuller and is open source code on{" "}
+      <a
+        href="https://github.com/eweymuller/dictionary-application"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github
+      </a>
+    </footer>
   </React.StrictMode>
 );
 
